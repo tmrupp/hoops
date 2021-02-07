69173e1869364000d6b32b7203a458db9bf4c4f6
tmrupp <tmrupp@umich.edu>
2021-02-07 09:48:15 -0700

    Made jumping work

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     Rigidbody2D rigidbody;
+    float speed = 5.0f, jumpSpeed = 5.0f;
+    bool grounded;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    void OnCollisionEnter2D (Collision2D c) 
+    {
+        Debug.Log("entered a collider, tag=" + c.gameObject.tag + " go=" + c.gameObject.ToString());
+        grounded = (c.gameObject.tag == "Ground");
+    }
+
+    void OnCollisionExit2D (Collision2D c) 
+    {
+        Debug.Log("left a collider, tag=" + c.gameObject.tag);
+        grounded = !(c.gameObject.tag == "Ground");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-        rigidbody.AddForce(new Vector2(x, y));
+        // var y = Inpu;
+        var v = rigidbody.velocity;
+        v.x = Input.GetAxis("Horizontal") * speed;
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) {
+            v.y = v.y + jumpSpeed;
+        }
+        rigidbody.velocity = v;
     }
 }
