d8039417ed0f8c5eb1f90f9b9bfd4ad619338417
tmrupp <tmrupp@umich.edu>
2021-02-07 11:03:23 -0700

    added some stuff from other move script

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,25 +7,36 @@ public class MovementController : MonoBehaviour
     Rigidbody2D rigidbody;
     public float speed = 5.0f, jumpSpeed = 5.0f;
     bool grounded;
+    private Transform m_GroundCheck;                    // A position marking where to check if the player is grounded.
+    [SerializeField] private LayerMask m_WhatIsGround;  // A mask determining what is ground to the character
+    const float k_GroundedRadius = .01f;                 // Radius of the overlap circle to determine if grounded
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        m_GroundCheck = transform.Find("GroundCheck");
     }
 
     void OnCollisionEnter2D (Collision2D c) 
     {
-        Debug.Log("entered a collider, tag=" + c.gameObject.tag + " go=" + c.gameObject.ToString());
         if (c.gameObject.tag == "Ground")
             grounded = true;
     }
 
     void OnCollisionExit2D (Collision2D c) 
     {
-        Debug.Log("left a collider, tag=" + c.gameObject.tag);
         if (c.gameObject.tag == "Ground")
             grounded = false;
+    }
+
+    void FixedUpdate () {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++) {
+            if (colliders[i].gameObject != gameObject) {
+                grounded = true;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -34,7 +45,7 @@ public class MovementController : MonoBehaviour
         // var y = Inpu;
         var v = rigidbody.velocity;
         v.x = Input.GetAxis("Horizontal") * speed;
-        if (grounded && Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
             v.y = v.y + jumpSpeed;
         }
         rigidbody.velocity = v;
